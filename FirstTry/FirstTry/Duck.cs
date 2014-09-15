6eb3c231334e2dee00594f8b14b12010c69eccc0
Bohdan <te.dark@mail.ru>
2014-09-15 22:11:14 +0300

    created base abstract class, added flying and quacking behavior, created examples for testing

---
+++ FirstTry/FirstTry/Duck.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace FirstTry
+{
+    class Programm
+    {
+        static void Main()
+        {
+            var littleDuck = new RubberDuck();
+            var woodenDuck = new WoodenDuck();
+
+            Console.WriteLine("1st duck");
+            littleDuck.Fly();
+            littleDuck.Quack();
+            littleDuck.Swim();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("2nd duck");
+            woodenDuck.Fly();
+            woodenDuck.Quack();
+            woodenDuck.Swim();
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Duck
+    {
+        public abstract void Swim();
+    }
+
+    public class FlyBehavior
+    {
+        public virtual void Fly()
+        {
+            Console.WriteLine("I'm flying");
+        }
+    }
+
+    public class FlyWithWings : FlyBehavior
+    {
+        public override void Fly()
+        {
+            Console.WriteLine("With wings!");
+        }
+    }
+
+    public class FlyWithOutWings : FlyBehavior
+    {
+        public override void Fly()
+        {
+            Console.WriteLine("I can't fly!");
+        }
+    }
+
+    public class QuackBehavior
+    {
+        public virtual void Quack()
+        {
+            Console.WriteLine("I'm quacking");
+        }
+    }
+
+    public class MuteQuack : QuackBehavior
+    {
+        public override void Quack()
+        {
+            Console.WriteLine("....");
+        }
+    }
+
+    public class Squeack : QuackBehavior
+    {
+        public override void Quack()
+        {
+            Console.WriteLine("Pew! Pew! Pew!!!");
+        }
+    }
+
+    public class RubberDuck : Duck
+    {
+        public override void Swim()
+        {
+            Console.WriteLine("I'm swimming");
+        }
+
+        public void Fly()
+        {
+            var tryFly = new FlyWithOutWings();
+            tryFly.Fly();
+        }
+
+        public void Quack()
+        {
+            var tryQuack = new Squeack();
+            tryQuack.Quack();
+        }
+    }
+
+    public class WoodenDuck : Duck
+    {
+        public override void Swim()
+        {
+            Console.WriteLine("I'm swimming");
+        }
+
+        public void Fly()
+        {
+            var tryFly = new FlyWithOutWings();
+            tryFly.Fly();
+        }
+
+        public void Quack()
+        {
+            var tryQuack = new MuteQuack();
+            tryQuack.Quack();
+        }
+    }
+}
