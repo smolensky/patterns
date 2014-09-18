4e1b9404d877a0866173311c48710a4873f4ca84
Bohdan <te.dark@mail.ru>
2014-09-18 21:10:19 +0300

    created "chocolate boiler", constructor made private to remove creating objects possibility

---
+++ FirstTry/ThirdTry/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ThirdTry.singletonPattern;
+
+namespace ThirdTry
+{
+    class Program
+    {
+        static void Main()
+        {
+            CheckNull();
+
+            ChocolateBoiler boiler = ChocolateBoiler.GetInstance();
+
+            CheckNull();
+
+            //should work correct
+            boiler.Fill();
+            boiler.Boil();
+            boiler.Drain();
+
+            Console.WriteLine("\n");
+
+            //should work incorrect
+            boiler.Boil();
+            boiler.Drain();
+            boiler.Fill();
+            boiler.Fill();
+            boiler.Drain();
+
+            Console.ReadKey();
+        }
+
+        private static void CheckNull()
+        {
+            Console.WriteLine(ChocolateBoiler._uniqueInstance == null
+                ? "You have null here! \n Create an object!"
+                : "You've created an object! \n You have no null anymore!");
+        }
+    }
+}
---
+++ FirstTry/ThirdTry/singletonPattern/ChocolateBoiler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ThirdTry.singletonPattern
+{
+    class ChocolateBoiler
+    {
+        public static ChocolateBoiler _uniqueInstance;
+        private bool _empty;
+        private bool _boiled;
+
+
+        private ChocolateBoiler()
+        {
+            _empty = true;
+            _boiled = false;
+        }
+
+        public static ChocolateBoiler GetInstance()
+        {
+            if (_uniqueInstance == null)
+                return _uniqueInstance = new ChocolateBoiler();
+            return _uniqueInstance;
+        }
+
+        public void Fill()
+        {
+            if (IsEmpty())
+            {
+                _empty = false;
+                _boiled = false;
+                Console.WriteLine("Filling . . .");
+            }
+            else
+                Console.WriteLine("Is full!!!");
+        }
+
+        public void Drain()
+        {
+            if (!IsEmpty() && IsBoiled())
+            {
+                _empty = true;
+                Console.WriteLine("Draining . . .");
+            }
+            else
+                Console.WriteLine("Nothing to drain or not boiled!!!");
+        }
+
+        public void Boil()
+        {
+            if (!IsEmpty() && !IsBoiled())
+            {
+                _boiled = true;
+                Console.WriteLine("Boiling . . .");
+            }
+            else
+                Console.WriteLine("Empty or already boiled!!!");
+        }
+
+        public bool IsEmpty()
+        {
+            return _empty;
+        }
+
+        public bool IsBoiled()
+        {
+            return _boiled;
+        }
+    }
+}
