889d96d89296dd481582fdd434d052623f48e49d
Bohdan <te.dark@mail.ru>
2014-09-23 18:27:33 +0300

    separeted facade interface and class

---
+++ FirstTry/adapterPattern/adapterPattern/DuckAndTurkeyFacade.cs
@@ -0,0 +1,17 @@
+﻿namespace adapterPattern.adapterPattern
+{
+    public class DuckAndTurkeyFacade : IDuckFacade
+    {
+        public void DoThings()
+        {
+            var simpleDuck = new SimpleDuck();
+            var simpleTurkey = new SimpleTurkey();
+
+            simpleTurkey.Gobble();
+            simpleTurkey.Fly();
+
+            simpleDuck.Quack();
+            simpleDuck.Fly();
+        }
+    }
+}
--- FirstTry/adapterPattern/adapterPattern/IDuckFacade.cs
+++ FirstTry/adapterPattern/adapterPattern/IDuckFacade.cs
@@ -4,19 +4,4 @@
     {
         void DoThings();
     }
-
-    public class DuckAndTurkeyFacade : IDuckFacade
-    {
-        public void DoThings()
-        {
-            var simpleDuck = new SimpleDuck();
-            var simpleTurkey = new SimpleTurkey();
-
-            simpleTurkey.Gobble();
-            simpleTurkey.Fly();
-
-            simpleDuck.Quack();
-            simpleDuck.Fly();
-        }
-    }
 }
